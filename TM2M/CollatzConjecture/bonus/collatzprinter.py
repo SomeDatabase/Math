8f8fc9b6018a4feb93541852c6a0c6e3a8f03917
SomeDatabase <38416172+SomeDatabase@users.noreply.github.com>
2018-10-10 13:35:10 -0400

    Update collatzprinter.py

--- TM2M/CollatzConjecture/bonus/collatzprinter.py
+++ TM2M/CollatzConjecture/bonus/collatzprinter.py
@@ -6,13 +6,13 @@ def collatz(n):
     while n > 1:
         if n % 2 == 0:
             n = n / 2
-            print(str(int(n)) + "\n")
+            print(str(int(n)))
         else:
             n = (3 * n)+ 1
-            print(str(int(n)) + "\n")
+            print(str(int(n)))
 
 def main():
-    startNumber = 67 #change this to whatever you want
+    startNumber = int(input("Pick a starting number: "))
     collatz(startNumber)
 
 main()
